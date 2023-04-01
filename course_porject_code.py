684b06229c369db5914c71eedf1434c052c4474f
anastasiajiv <129009401+anastasiajiv@users.noreply.github.com>
2023-04-01 15:42:32 -0400

    Add files via upload

--- course_porject_code.py
+++ course_porject_code.py
@@ -21,8 +21,9 @@ frame1.pack(padx=5, pady=5)
 questions = ['What genre of movie do you want to watch?', 'Length of Movie?', 'Preference for rating of movie?',
              'What Language do you want the movie to be in?',
              'Do you have a preference for what year the movie should be released?']
-answers = [['Romance', 'Comedy', 'Action', 'Horror', 'Thriller'], ['< 1h 40 min', '>= 1h 40 min'],
-           ['High: 8-10', ' Mediocre: 5-7',  'Low: 1-4', 'No preference'], ['English', 'Japanese', 'Italian'],
+answers = [['Romance', 'Comedy', 'Action', 'Horror', 'Thriller', 'Adventure', 'Crime', 'Animation',
+           'Science Fiction', 'Family', 'Drama', 'Music', 'Skip'], ['< 1h 40 min', '>= 1h 40 min', 'Skip'],
+           ['High: 8-10', ' Mediocre: 5-7',  'Low: 1-4', 'No preference'], ['English', 'Another Language', 'Skip'],
            ['2021', '2016-2020', '<2016', 'Skip']]
 
 # We use IntVar to keep track of the choice and number that the user chose so that we can later put this in a list
@@ -32,9 +33,40 @@ answers = [['Romance', 'Comedy', 'Action', 'Horror', 'Thriller'], ['< 1h 40 min'
 # Question 1:
 tk.Label(frame1, text='What genre of movie do you want to watch? Select one from the box below.').pack(anchor='w')
 # Choices for question 1
-options = ['Romance', 'Comedy', 'Action', 'Horror', 'Thriller', 'Action', 'Skip']
+options = ['Romance', 'Comedy', 'Action', 'Horror', 'Thriller', 'Adventure', 'Crime', 'Animation',
+           'Science Fiction', 'Family', 'Drama', 'Music', 'Skip']
 choice = StringVar()
 drop_box = OptionMenu(frame1, choice, *options)
+
+def choice_q_1(answer: str) -> int:
+    """Takes the string value for the choice the user made and turns it into a unique integer."""
+    if answer == 'Romance':
+        return 0
+    elif answer == 'Comedy':
+        return 1
+    elif answer == 'Action':
+        return 2
+    elif answer == 'Horror':
+        return 3
+    elif answer == 'Thriller':
+        return 4
+    elif answer == 'Adventure':
+        return 5
+    elif answer == 'Crime':
+        return 6
+    elif answer == 'Animation':
+        return 7
+    elif answer == 'Science Fiction':
+        return 8
+    elif answer == 'Family':
+        return 9
+    elif answer == 'Drama':
+        return 10
+    elif answer == 'Music':
+        return 11
+    else:
+        return 12
+
 drop_box.pack(anchor='w')
 
 #frame3 = Frame(main)
@@ -48,7 +80,15 @@ tk.Radiobutton(frame1, text='< 1h 40 min', variable=choice_q_2, value='< 1h 40 m
 tk.Radiobutton(frame1, text='>= 1h 40 min', variable=choice_q_2, value='>= 1h 40 min').pack(anchor='w')
 tk.Radiobutton(frame1, text='Skip', variable=choice_q_2, value='Skip').pack(anchor='w')
 
-#frame4 = Frame(main)
+def choices_q_2(answer: str) -> int:
+    """Takes the string value for the choice the user made and turns it into a unique integer."""
+    if answer == '< 1h 40 min':
+        return 13
+    elif answer == '>= 1h 40 min':
+        return 14
+    else:
+        return 15
+
 # Question 3:
 choice_q_3 = StringVar()
 choice_q_3.set('High: 8-10')
@@ -57,52 +97,66 @@ tk.Radiobutton(frame1, text='High: 8-10', variable=choice_q_3, value='High: 8-10
 tk.Radiobutton(frame1, text='Mediocre: 5-7', variable=choice_q_3, value='Mediocre: 5-7').pack(anchor='w')
 tk.Radiobutton(frame1, text='Low: 1-4', variable=choice_q_3, value='Low: 1-4').pack(anchor='w')
 tk.Radiobutton(frame1, text='Skip', variable=choice_q_3, value='Skip').pack(anchor='w')
-#frame4.pack(padx=5, pady=5)
 
-#frame5 = Frame(main)
+def choices_q_3(answer: str) -> int:
+    """Takes the string value for the choice the user made and turns it into a unique integer."""
+    if answer == 'High: 8-10':
+        return 16
+    elif answer == 'Mediocre: 5-7.9':
+        return 17
+    elif answer == 'Low: 1-4.9':
+        return 18
+    else:
+        return 19
+
 # Question 4
-tk.Label(frame1, text='What language do you want the movie to be in?').pack(anchor='w')
-choices = ['English', 'Japanese', 'Italian', 'French', 'No Preference', 'Skip']
 choice_q_4 = StringVar()
-drop_box_2 = OptionMenu(frame1, choice_q_4, *choices)
-drop_box_2.pack(anchor='w')
-#frame5.pack(padx=5, pady=5)
+choice_q_4.set('English')
+tk.Label(frame1, text='What language do you want the movie to be in?').pack(anchor='w')
+choices = ['English', 'Another Language', 'Skip']
+tk.Radiobutton(frame1, text='English', variable=choice_q_4, value='English').pack(anchor='w')
+tk.Radiobutton(frame1, text='Another Language', variable=choice_q_4, value='Another Language').pack(anchor='w')
+tk.Radiobutton(frame1, text='Skip', variable=choice_q_4, value='Skip').pack(anchor='w')
 
-#frame6 = Frame(main)
+def choices_q_4(answer: str) -> int:
+    """Takes the string value for the choice the user made and turns it into a unique integer."""
+    if answer == 'English':
+        return 20
+    elif answer == 'Another Language':
+        return 21
+    else:
+        return 22
+
 # Question 5
 choice_q_5 = StringVar()
-choice_q_5.set('2021')
+choice_q_5.set('>=2021')
 tk.Label(frame1, text='Do you have a preference for what year the movie should be released?').pack(anchor='w')
-tk.Radiobutton(frame1, text='2021', variable=choice_q_5, value='2021').pack(anchor='w')
+tk.Radiobutton(frame1, text='>=2021', variable=choice_q_5, value='>=2021').pack(anchor='w')
 tk.Radiobutton(frame1, text='2016-2020', variable=choice_q_5, value='2016-2020').pack(anchor='w')
 tk.Radiobutton(frame1, text='<2016', variable=choice_q_5, value='<2016').pack(anchor='w')
 tk.Radiobutton(frame1, text='Skip', variable=choice_q_5, value='Skip').pack(anchor='w')
-#frame6.pack(padx=5, pady=5)
-# Question 6
+
+def choices_q_5(answer: str) -> int:
+    """Takes the string value for the choice the user made and turns it into a unique integer."""
+    if answer == '>=2021':
+        return 23
+    elif answer == '2016-2020':
+        return 24
+    elif answer =='<2016':
+        return 25
+    else:
+        return 26
 
 def clicked():
     """When the done button is clicked switch the text to loading."""
     quit_when_done.config(text='LOADING...')
     print([choice.get()] + [choice_q_2.get()] + [choice_q_3.get()] + [choice_q_4.get()]
           + [choice_q_5.get()])
+    print(choice_q_1(choice.get()) + choices_q_2(choice_q_2.get()) + choices_q_3(choice_q_3.get())
+          + choices_q_4(choice_q_4.get()) + choices_q_5(choice_q_5.get()))
 
 quit_when_done = Button(main, text='DONE', command=clicked)
 quit_when_done.pack(padx=5, pady=5)
-
-import csv
-# function to sort through the words in the plot
-def word_tally(csv_lst: str) -> dict[str, int]:
-    """Returns a tally for each word in the plot of each movie"""
-    # index 9
-    dict_so_far = {}
-    lst = csv_lst.split()
-    for word in lst:
-        if word not in dict_so_far:
-            dict_so_far[word] = 1
-        else:
-            dict_so_far[word] = dict_so_far[word] + 1
-    return dict_so_far
-
 
 # loop over python code
 main.mainloop()
