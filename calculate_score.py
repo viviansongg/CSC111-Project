54f1c588b8ae7531216269e70d6cfd6ee7ad1ff2
anastasiajiv <129009401+anastasiajiv@users.noreply.github.com>
2023-04-02 17:01:39 -0400

    Add files via upload

--- calculate_score.py
+++ calculate_score.py
@@ -1,7 +1,9 @@
-"""Computing Scores for Movies
+"""Computing Scores for Movies for a dataset.
 """
-import random
-def calculate_score(movie_file: str) -> dict:
+import csv
+
+
+def calculate_score1(movie_file: str) -> dict:
     """Returns a dictionary mapping each movie in the csv file to its associated score.
     """
     movie_scores = {}
@@ -11,14 +13,39 @@ def calculate_score(movie_file: str) -> dict:
             # question 1
             genre = row[5]
             genre = genre.strip("[']")
-            answer1 = choices_q_1(genre)
+            if genre == 'Romance':
+                answer1 = 100000
+            elif genre == 'Comedy':
+                answer1 = 200000
+            elif genre == 'Action':
+                answer1 = 300000
+            elif genre == 'Horror':
+                answer1 = 400000
+            elif genre == 'Thriller':
+                answer1 = 500000
+            elif genre == 'Adventure':
+                answer1 = 600000
+            elif genre == 'Crime':
+                answer1 = 700000
+            elif genre == 'Animation':
+                answer1 = 800000
+            elif genre == 'Science Fiction':
+                answer1 = 900000
+            elif genre == 'Family':
+                answer1 = 1000000
+            elif genre == 'Drama':
+                answer1 = 1100000
+            elif genre == 'Western':
+                answer1 = 1200000
+            else:
+                answer1 = 1300000
 
             # question 2
             if int(row[4]) < 100:
                 answer2 = 100
             else:
                 answer2 = 200
-            
+
             # question 3
             if float(row[3]) >= 8.0 and float(row[3]) <= 10.0:
                 answer3 = 1000
@@ -26,7 +53,7 @@ def calculate_score(movie_file: str) -> dict:
                 answer3 = 2000
             else:
                 answer3 = 3000
-            
+
             # question 4
             if row[0] == 'en':
                 answer4 = 10000
@@ -35,11 +62,11 @@ def calculate_score(movie_file: str) -> dict:
 
             # question 5
             if row[2][:4] >= '2021':
-                answer5 = 100000
+                answer5 = 1
             elif row[2][:4] >= '2016' and row[2][:4] <= '2020':
-                answer5 = 200000
+                answer5 = 2
             else:
-                answer5 = 300000
-            
+                answer5 = 3
+
             movie_scores[row[1]] = answer1 + answer2 + answer3 + answer4 + answer5
     return movie_scores
