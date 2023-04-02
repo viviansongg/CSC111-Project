2cc2183ffe6150d4e7936e51aecf4861cfd74785
anastasiajiv <129009401+anastasiajiv@users.noreply.github.com>
2023-04-01 21:29:58 -0400

    Update calculate_score.py

--- calculate_score.py
+++ calculate_score.py
@@ -1,6 +1,6 @@
 """Computing Scores for Movies
 """
-
+import random
 def calculate_score(movie_file: str) -> dict:
     """Returns a dictionary mapping each movie in the csv file to its associated score.
     """
@@ -16,21 +16,17 @@ def calculate_score(movie_file: str) -> dict:
             # question 2
             if int(row[4]) < 100:
                 answer2 = 100
-            elif int(row[4]) >= 100:
-                answer2 = 200
             else:
-                answer2 = 300
-
+                answer2 = 200
+            
             # question 3
             if float(row[3]) >= 8.0 and float(row[3]) <= 10.0:
                 answer3 = 1000
             elif float(row[3]) >= 5.0 and float(row[3]) <= 7.9:
                 answer3 = 2000
-            elif float(row[3]) >= 1.0 and float(row[3]) <= 4.9:
-                answer3 = 3000
             else:
-                answer3 = 4000
-
+                answer3 = 3000
+            
             # question 4
             if row[0] == 'en':
                 answer4 = 10000
@@ -42,9 +38,8 @@ def calculate_score(movie_file: str) -> dict:
                 answer5 = 100000
             elif row[2][:4] >= '2016' and row[2][:4] <= '2020':
                 answer5 = 200000
-            elif row[2][:4] < '2016':
-                answer5 = 300000
             else:
-                answer5 = 400000
+                answer5 = 300000
+            
             movie_scores[row[1]] = answer1 + answer2 + answer3 + answer4 + answer5
     return movie_scores
